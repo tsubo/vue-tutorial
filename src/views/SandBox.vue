5616a4c4407c7397ccee4a06b65ef859be28c4f0
tsubo <ktsubota@gmail.com>
2019-07-14 01:48:16 +0900

    単一ファイルコンポーネント

--- src/views/SandBox.vue
+++ src/views/SandBox.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="sandbox">
+    <!-- ここにこのコンポーネントの HTML を書きます -->
     <h1>SandBox</h1>
+
+    <p>{{ msg }}</p>
+    <p>{{ now() }}</p>
   </div>
 </template>
+
+<script>
+// ここに JavaScript を書きます
+
+// このコンポーネントで使用する Vue オブジェクト
+export default {
+  // データ
+  data() {
+    return {
+      msg: "Hello, World!"
+    };
+  },
+
+  // メソッド
+  methods: {
+    now() {
+      return new Date();
+    }
+  }
+};
+</script>
+
+<style>
+/* ここに、このコンポーネントに適用する CSS を書きます */
+.sandbox {
+  color: red;
+}
+</style>
